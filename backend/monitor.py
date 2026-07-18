fb941c080bc5be0790860dbbefbe4600ee598f8e
Gayathri-1028t <gayuelisa28@gmail.com>
2026-07-18 13:06:12 +0530

    Day 4: Implement real-time file monitoring with Watchdog

---
+++ backend/monitor.py
@@ -0,0 +1,44 @@
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+import time
+
+from hashing import generate_hash
+from compare_hash import compare_hash
+
+
+class FileMonitor(FileSystemEventHandler):
+
+    def on_modified(self, event):
+
+        if event.is_directory:
+            return
+
+        print("\n📁 File Modified:", event.src_path)
+
+        current_hash = generate_hash(event.src_path)
+
+        compare_hash("sample.txt", current_hash)
+
+
+observer = Observer()
+
+event_handler = FileMonitor()
+
+observer.schedule(
+    event_handler,
+    path="../test_files",
+    recursive=False
+)
+
+observer.start()
+
+print("👀 Nexora is monitoring files...")
+
+try:
+    while True:
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    observer.stop()
+
+observer.join()
